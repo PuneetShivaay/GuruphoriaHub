6c5af48bef13810583548f880cc26237065c2d01
Puneet Kumar <puneetshivaay@gmail.com>
2025-10-25 21:15:34 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'i.ytimg.com',
+        hostname: '**.ytimg.com',
         port: '',
         pathname: '/**',
       },
