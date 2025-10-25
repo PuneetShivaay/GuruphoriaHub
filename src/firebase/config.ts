873e69de752620bd35b6a53f8129bef8adf36dce
Guruphoria Institute <guruphoria@gmail.com>
2025-10-25 19:56:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9746372274-c8924",
+  "appId": "1:363545728201:web:cc421c84bf505c594f2918",
+  "apiKey": "AIzaSyCgaaKxdyb8Te1lFeYV35roCVCTDGIBtNs",
+  "authDomain": "studio-9746372274-c8924.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "363545728201"
+};
